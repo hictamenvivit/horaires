968ba9982db1fad9c8662f54bd9f4553f1ea54d7
Maxime <maxime.bettinelli@gmail.com>
2019-02-05 10:05:26 +0000

    probably shouldnt

--- front.py
+++ front.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from main import Allosession 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/files')
 
 @app.route('/')
 def index():
     a = Allosession()
     return a.as_html()
+    
+@app.route('/scenario/')
+def scenario():
+    return app.send_static_file('Maxime_Bettinelli_Le_Bulletin.pdf')
+    
