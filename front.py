edb479b2abd8c527f75df2ce45ab8561af2906a1
Maxime Bettinelli <maxime.bettinelli@gmail.com>
2019-11-11 17:31:39 +0100

    refactored to use vue

--- front.py
+++ front.py
@@ -1,12 +1,17 @@
-from flask import Flask, send_file
-from main import Allosession 
+from flask import Flask, send_file, render_template
+import json
+from models import get_formatted_showtimes
+
 
 app = Flask(__name__, static_url_path='/files/')
+app.config['JSON_AS_ASCII'] = False
 
 @app.route('/')
 def index():
-    a = Allosession()
-    return a.as_html()
+    with open('theater_codes.json', 'r') as f:
+        theater_codes = json.load(f)
+    showtimes = get_formatted_showtimes(theater_codes)
+    return render_template('hello.html', content=showtimes)
     
 @app.route('/scenario/')
 def scenario():
@@ -16,4 +21,8 @@ def scenario():
 @app.route('/hello/')
 def hello():
     return 'hello'
+    
+    
+if __name__ == "__main__":
+    app.run(debug=True)
     
---
+++ models.py
@@ -0,0 +1,53 @@
+from datetime import datetime as dt
+import json
+from allocine import *
+
+
+
+def get_formatted_showtimes(theaters_dict, jour_choisi=dt.strftime(dt.today(),"%Y-%m-%d")):
+    theaters_codes = ','.join(theaters_dict.keys())
+    
+    stimes = showtimelist(theaters=theaters_codes)
+
+    theaterstimes = stimes['feed']['theaterShowtimes']
+
+    result = dict()
+    for cinema in theaterstimes:
+        code_cine = cinema['place']['theater']['code']
+        nom_cine = theaters_dict[code_cine]
+        liste_films = []
+        films = cinema['movieShowtimes']
+        for film in films:
+            if 'scr' not in film.keys():
+                continue
+            scr = film['scr'][0]
+            date = scr['d']
+            if date == jour_choisi:
+                ce_film = {
+                    'titre': film['onShow']['movie']['title'],
+                    'horaires':[i['$'] for i in scr['t'] ],
+                    'directors': film['onShow']['movie']['castingShort']['directors']
+                }
+                ce_film['join_horaires'] = ' | '.join(ce_film['horaires'])
+                liste_films.append(ce_film)
+        result[nom_cine] = liste_films
+    return result
+    
+noms_simples = {
+'C0020':'La Filmo',
+'C0054':"L'Arlequin",
+'C0073':'Le Champo',
+'C0074':'Reflet Médicis',
+'W7510':'Le Louxor',
+'C0089':'Max Linder',
+'C0102':'UGC Danton',
+'C0104':'UGC Odéon',
+'C0097':'MK2 Odéon (St Germain)',
+'C0092':'MK2 Odéon (St Michel)',
+'C0103':'UGC Montparnasse',
+'C0105':'UGC Rotonde',
+}
+
+
+
+
